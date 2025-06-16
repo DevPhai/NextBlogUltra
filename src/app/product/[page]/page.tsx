b74a9620014365b2088de11723964d2e12743f5e
DevPhai <thapana10@gmail.com>
2025-06-16 10:56:21 +0700

    name fit

--- src/app/product/[page]/page.tsx
+++ src/app/product/[page]/page.tsx
@@ -1,5 +1,5 @@
 export async function generateStaticParams() {
-  const page = ['1', '2', '3'];
+  const page = ["1", "2", "3"];
 
   return page.map((page) => ({
     page,
@@ -8,8 +8,8 @@ export async function generateStaticParams() {
 export default async function Page({
   params,
 }: {
-  params: Promise<{ page: string }>
+  params: Promise<{ page: string }>;
 }) {
-  const { page } = await params
+  const { page } = await params;
   return <div>Article: {page}</div>;
 }
